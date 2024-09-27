b125d3ec79d288b619ec2f5bcd2aacc29315022f
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-09-27 18:35:42 +0530

    .

--- djsnss-web-frontend/vite.config.js
+++ djsnss-web-frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()],
-  root: 'src', // Set root if your HTML file is in the src folder
+  root: '.', // Point to the src directory for index.html
   build: {
-    outDir: '../dist', // Specify output directory relative to the root
+    outDir: '../dist', // Ensure that the build output goes to the correct folder
   },
 });
